d20ad3822305c3540379add14550cb8e8ee45ae6
HRITHIK1117 <79209084+HRITHIK1117@users.noreply.github.com>
2021-07-06 12:16:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var playerPaddle;
+var computerPaddle;
+
+
+function setup(){
+  createCanvas(400,400);
+  //player Paddle
+  playerPaddle = new Paddle();
+  computerPaddle = new Paddle();
+}
+
+function draw() {
+  //set background to white
+  background("white");
+  
+  playerPaddle.xPosition = 390;
+  playerPaddle.yPosition = mouseY;
+  
+  playerPaddle.display();
+  computerPaddle.display();
+  
+  //draw the ball
+  rect(200,200,10,10);
+}
